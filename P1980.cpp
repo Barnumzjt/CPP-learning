f22007e351e27c53dc2af69631a1422b6ff088e0
Barnumzjt <2641105495@qq.com>
2026-05-27 22:07:54 +0800

    Create P1980.cpp

---
+++ P1980.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char x;
+    cin>>x;
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        string s=to_string(i);
+        for(char c:s){
+            if(c==x)ans++;
+        }
+    }
+    cout<<ans;
+    return 0;
+}
